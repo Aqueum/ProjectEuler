b2f891753378cc556f1ed28a4422351c7e1f4bf3
Aqueum <martin@aqueum.com>
2022-02-20 12:40:17 +0000

    sum answers

--- 2 Even Fibonacci/solution.py
+++ 2 Even Fibonacci/solution.py
@@ -1,9 +1,11 @@
 x = 1
 y = 2
-print(y)
+z = y
 while x < 4000000:
   next = x + y
   x = y
   y = next
   if next < 4000000 and next % 2 == 0:
     print(next)
+    z += next
+print("answer = ", z)
