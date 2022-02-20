d0a210c63406d845b9e0b8568e781c0c2d4a07b7
Aqueum <martin@aqueum.com>
2022-02-20 11:37:38 +0000

    set up multiple function

--- 1 Multiples of 3 or 5/solution.py
+++ 1 Multiples of 3 or 5/solution.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from operator import truediv
+
+
+def multiple(number, divisor):
+  if number % divisor == 0:
+    return True
+  else:
+    return False
+
+print(multiple(5,2))
