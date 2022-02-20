e0b7c87e3f4461f0a84cb4c016d6d43b6d499ada
Aqueum <martin@aqueum.com>
2022-02-20 11:41:55 +0000

    document

--- 1 Multiples of 3 or 5/solution.py
+++ 1 Multiples of 3 or 5/solution.py
@@ -1,6 +1,6 @@
-from operator import truediv
-
-
+# a function that takes arguments number and divisor
+# and returns true if number is multiple of divisor
+# otherwise false
 def multiple(number, divisor):
   if number % divisor == 0:
     return True
